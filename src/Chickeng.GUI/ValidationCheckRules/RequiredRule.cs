45f5dffa770ce3dba39351a7e28f596cf8bf24f2
nstungdev <nstung.dev@gmail.com>
2023-02-26 16:18:11 +0700

    Need TODO

---
+++ src/Chickeng.GUI/ValidationCheckRules/RequiredRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Chickeng.GUI.ValidationCheckRules
+{
+    public class RequiredRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return new ValidationResult(false, $"The field is required");
+            return ValidationResult.ValidResult;
+        }
+    }
+}
